f0576fe8bed2415e544e54b44e30412414577b9b
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-05-15 16:46:26 +0300

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,6 +7,9 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
     const isUserLoggedIn = true;
+
+    const [providers, setProviders] = useState(null);
+
     return (
         <nav className='flex-between w-full mb-16 pt-3'>
             <Link href="/" className="flex gap-2 flex-center">
@@ -27,6 +30,20 @@ const Nav = () => {
                         className="black_btn"
                         >
                             Create Post
+                        </Link>
+                        <button type="button" onClick={signOut} 
+                        className="outline_btn">
+                            Sign Out
+                        </button>
+
+                        <Link href="/profile">
+                            <Image 
+                                src="/assets/images/logo.svg"
+                                width={37}
+                                height={37}
+                                className="rounded-full"
+                                alt="profile"
+                            />
                         </Link>
                     </div>
                 ) : (
