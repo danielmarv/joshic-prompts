127b3593b436633ffce7ad08fcffa71a81f1b996
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-05-15 16:32:03 +0300

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,12 +1,42 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
+    const isUserLoggedIn = true;
+    return (
+        <nav className='flex-between w-full mb-16 pt-3'>
+            <Link href="/" className="flex gap-2 flex-center">
+                <Image 
+                    src="/assets/images/logo.svg"
+                    alt="JPrompt Logo"
+                    width={30}
+                    height={30}
+                    className="object-contain"
+                />
+                <p className="logo_text">JPrompt</p>
+            </Link>
+            {/* Mobile Navigation */}
+            <div className="sm:flex hidden">
+                {isUserLoggedIn ? (
+                    <div className="flex gap-3 md:gap-5">
+                        <Link href="/create-prompt"
+                        className="black_btn"
+                        >
+                            Create Post
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+
+                    </>
+                )}
+            </div>
+        </nav>
+    )
 }
 
 export default Nav
