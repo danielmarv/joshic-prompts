ce29cb658691dfb6f481c6406c6fabd51e253c00
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-05-22 18:00:13 +0300

    Creating Prompts Page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
@@ -7,17 +7,46 @@ import { useRouter } from 'next/navigation';
 import Form from '@components/Form';
 
 const CreatePrompt = () => {
+    const router = useRouter();
+    const { data: session } = useSession();
+
     const [submitting, setSubmitting] = useState();
     const [post, setPost] = useState({
-        promt: '',
+        prompt: '',
         tag: '',
     });
 
     const createPrompt = async (e) => {
-        
+        e.preventDefault();
+        setSubmitting(true);
+
+        try {
+            const response = await fetch('/api/prompt/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag
+                })
+            })
+
+            if(response.ok) {
+                router.push('/')
+            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setSubmitting(false);
+        }
     }
     return (
-        <Form />
+        <Form
+            type="Create"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={createPrompt}
+        />
     )
 }
 
