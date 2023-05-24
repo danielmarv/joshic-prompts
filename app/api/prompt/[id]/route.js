504d509c1582aa0f5b10f1c835f028a1168ffeb1
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-05-24 10:45:09 +0300

    Update route.js

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,6 +1,7 @@
 import { connectToDB } from '@utils/database';
 import Prompt from '@models/prompt';
 
+//GET (read)
 export const GET = async (request, { params }) => {
     try {
         await connectToDB();
@@ -14,6 +15,7 @@ export const GET = async (request, { params }) => {
     }
 }
 
+//PATCH (update)
 export const PATCH = async (request, { params }) => {
     const { prompt, tag } = await request.json();
 
@@ -21,7 +23,30 @@ export const PATCH = async (request, { params }) => {
         await connectToDB();
 
         const existingPrompt = await Prompt.findById(params.id);
+
+        if(!existingPrompt) return new Response("Prompt not found", { status: 404});
+
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+
+        return new Response(JSON.stringify(existingPrompt), { status: 200});
     } catch (error) {
+        return new Response("Failed to update prompt", { status: 500})
         
     }
 }
+
+//DELETE (delete)
+export const DELETE = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+    await Prompt.findByIdAndRemove(params.id);
+
+    return new Response("Prompt deleted successfully", { status: 200 });
+    } catch (error) {
+        return new Response("Failed to delete prompt", { status: 500 });
+    }
+};
